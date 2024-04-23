b2fb053405cf5d8a54eb0aad3707b732d0ae7cd0
22IB28_SRAMEK <22IB28_SRAMEK@U1-10>
2024-04-23 08:36:07 +0200

    Přidejte soubory projektu.

---
+++ databazePotraviny/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using databazePotraviny;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabazePotraviny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Polozka> Polozky { get; set; }
+        public MainWindow()
+        {
+            Polozky = new();
+            InitializeComponent();
+            Seznam.ItemsSource = Polozky;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OknoPridat dialog = new(Polozky);
+            dialog.Closing += Dialog_Closing;
+            dialog.ShowDialog();
+        }
+        private void Dialog_Closing(object? sender, EventArgs e)
+        {
+            Seznam.ItemsSource = null;
+            Seznam.ItemsSource = Polozky;
+        }
+
+    }
+}
---
+++ databazePotraviny/Polozka.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databazePotraviny
+{
+    public class Polozka
+    {
+        public Polozka(int? iD, string nazev, string cena, int pocet)
+        {
+            ID = iD;
+            Nazev = nazev;
+            Cena = cena;
+            this.pocet = pocet;
+        }
+
+        public int? ID { get; set; }
+        public string Nazev { get; set; }
+        public string Cena { get; set; }
+        public int pocet {  get; set; }
+    }
+}
